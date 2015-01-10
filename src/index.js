7c3701154bed1589564d485c033f092f77c86e34
nathanfaucett <nathanfaucett@gmail.com>
2015-01-10 11:31:56 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-var environment = module.exports;
+var environment = module.exports,
+
+    hasWindow = typeof(window) !== "undefined",
+    userAgent = hasWindow ? window.navigator.userAgent : "";
 
 
 environment.browser = !!(
-    typeof(window) !== "undefined" &&
+    hasWindow &&
     typeof(navigator) !== "undefined" &&
     window.document
 );
 
 environment.node = !environment.browser;
 
+environment.mobile = environment.browser && /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
+
 environment.window = (
-    typeof(window) !== "undefined" ? window :
+    hasWindow ? window :
     typeof(global) !== "undefined" ? global :
     typeof(self) !== "undefined" ? self : {}
 );
